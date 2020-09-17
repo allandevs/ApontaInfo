74f6f9b4111e9964e2f6345e653108c0e00e0392
Allan Oliveira <allan_rj5@hotmail.com>
2020-09-17 14:58:15 -0300

    sistema web de aponta melhores preços

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { BannerComponent } from './banner/banner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopoComponent,
+    HomeComponent,
+    RodapeComponent,
+    BannerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
