b490c07df5c3c7e4334e418a78b5757054312761
Allan Oliveira <allan_rj5@hotmail.com>
2020-09-20 16:33:19 -0300

    ajuste service ofertas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service'
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers:[OfertasService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[]
+
+  constructor( private ofertasService: OfertasService ) { }
 
   ngOnInit(): void {
+
+   console.log(this.ofertas =  this.ofertasService.getOfertas())
   }
+
+
 
 }
