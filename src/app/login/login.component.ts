c064ad6c0b276adb66755b88f008ed8353b10a96
Allan Oliveira <allan_rj5@hotmail.com>
2020-11-02 22:49:10 -0300

    tela de login empresa

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor( public router: Router,) { }
+
+  ngOnInit(): void {
+  }
+ login(){
+  this.router.navigate(['painel']);
+ }
+}
