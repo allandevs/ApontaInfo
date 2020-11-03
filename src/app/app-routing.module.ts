c9dd76ae7a77af56b65e983cef057299dffef0ff
Allan Oliveira <allan_rj5@hotmail.com>
2020-11-02 22:51:22 -0300

    criaçao de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CadastroComponent } from  './cadastro/cadastro.component'
+import { LoginComponent } from './login/login.component'
+import { PainelComponent } from './empresa/painel/painel.component'
+import { FuncionaComponent } from './funciona/funciona.component'
 
-const routes: Routes = [];
+// const routes: Routes = [];
+
+
+const ROUTES: Routes = [
+{ path:'cadastro', component: CadastroComponent },
+{ path:'login', component: LoginComponent },
+{ path: 'painel', component: PainelComponent },
+{ path: 'funciona', component: FuncionaComponent },
+{
+   path:'',
+    component: HomeComponent,
+    children: [
+      
+    ],
+  },
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
