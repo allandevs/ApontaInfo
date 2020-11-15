dcaf4ec8e17b315233888b69eaaf11c0ccdeb30a
Allan Oliveira <allan_rj5@hotmail.com>
2020-11-15 20:21:46 -0300

    edição tamanho imagem  produto e validação cnpj

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -38,81 +38,77 @@ export class CadastroComponent implements OnInit {
   }
 
   cadastrar(){
-   this.cadastroUser.cadastrarEmpresa(this.formCadastro.value).subscribe(
-
-     (result :any ) =>{
-      this.suporte.showMessage('Parabéns cadastrado realizado com sucesso!')
-      this.router.navigate(['login']);
-      
-   },(error) => {
-    this.suporte.showMessageErro('HOUVE UM ERRO')})
-    // this.validarCNPJ(this.cnpj)
+    this.validarCNPJ(this.cnpj)
   }
 
 
 
-//   validarCNPJ(cnpj) {
-//     cnpj = cnpj.replace(/[^\d]+/g,'');
+  validarCNPJ(cnpj) {
+
+    cnpj = cnpj.replace(/[^\d]+/g,'');
  
-//     if(cnpj == '') {
-//       console.log('invalido')
-//       this.suporte.showMessageErro('CNPJ INVálido')
-//     }
-     
-//     if (cnpj.length != 14){
-//       console.log('invalido')
-//       this.suporte.showMessageErro('CNPJ INVálido')
-//     }
-  
- 
-//     // Elimina CNPJs invalidos conhecidos
-//     if (cnpj == "00000000000000" || 
-//         cnpj == "11111111111111" || 
-//         cnpj == "22222222222222" || 
-//         cnpj == "33333333333333" || 
-//         cnpj == "44444444444444" || 
-//         cnpj == "55555555555555" || 
-//         cnpj == "66666666666666" || 
-//         cnpj == "77777777777777" || 
-//         cnpj == "88888888888888" || 
-//         cnpj == "99999999999999"){
-//         console.log('invalido')
-//         this.suporte.showMessageErro('CNPJ INVálido')
-//         }
+    if(cnpj == '') {
+      console.log('invalido')
+      this.suporte.showMessageErro('CNPJ inválido')
+    } else if  (cnpj.length != 14){
+      console.log('invalido')
+      this.suporte.showMessageErro('CNPJ inválido')
+    } else if (cnpj == "00000000000000" || 
+        cnpj == "11111111111111" || 
+        cnpj == "22222222222222" || 
+        cnpj == "33333333333333" || 
+        cnpj == "44444444444444" || 
+        cnpj == "55555555555555" || 
+        cnpj == "66666666666666" || 
+        cnpj == "77777777777777" || 
+        cnpj == "88888888888888" || 
+        cnpj == "99999999999999"){
+        console.log('invalido')
+        this.suporte.showMessageErro('CNPJ inválido')
+        } else {
+          this.cadastroUser.cadastrarEmpresa(this.formCadastro.value).subscribe(
+
+            (result :any ) =>{
+             this.suporte.showMessage('Parabéns cadastrado realizado com sucesso!')
+             this.router.navigate(['login']);
+             
+          },(error) => {
+           this.suporte.showMessageErro('HOUVE UM ERRO')})
+        }
         
          
-//     // Valida DVs
-//    var tamanho = cnpj.length - 2
-//    var numeros = cnpj.substring(0,tamanho);
-//    var digitos = cnpj.substring(tamanho);
-//    var soma = 0;
-//    var pos = tamanho - 7;
-//     for (i = tamanho; i >= 1; i--) {
-//      soma += numeros.charAt(tamanho - i) * pos--;
-//       if (pos < 2)
-//             pos = 9;
-//     }
-//    var  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-//     if (resultado != digitos.charAt(0))
-//         return false;
+  //   // Valida DVs
+  //  var tamanho = cnpj.length - 2
+  //  var numeros = cnpj.substring(0,tamanho);
+  //  var digitos = cnpj.substring(tamanho);
+  //  var soma = 0;
+  //  var pos = tamanho - 7;
+  //   for (i = tamanho; i >= 1; i--) {
+  //    soma += numeros.charAt(tamanho - i) * pos--;
+  //     if (pos < 2)
+  //           pos = 9;
+  //   }
+  //  var  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  //   if (resultado != digitos.charAt(0))
+  //       return false;
          
-//     tamanho = tamanho + 1;
-//     numeros = cnpj.substring(0,tamanho);
-//     soma = 0;
-//     pos = tamanho - 7;
-//     for (var i = tamanho; i >= 1; i--) {
-//       soma += numeros.charAt(tamanho - i) * pos--;
-//       if (pos < 2)
-//             pos = 9;
-//     }
-//     resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-//     if (resultado != digitos.charAt(1)){
-//       return false
-//     }else{
-//       this.suporte.showMessage('CNPJ valido')
-//     }
+  //   tamanho = tamanho + 1;
+  //   numeros = cnpj.substring(0,tamanho);
+  //   soma = 0;
+  //   pos = tamanho - 7;
+  //   for (var i = tamanho; i >= 1; i--) {
+  //     soma += numeros.charAt(tamanho - i) * pos--;
+  //     if (pos < 2)
+  //           pos = 9;
+  //   }
+  //   resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  //   if (resultado != digitos.charAt(1)){
+  //     return false
+  //   }else{
+  //     this.suporte.showMessage('CNPJ valido')
+  //   }
           
     
-//  }
+ }
 
 }
