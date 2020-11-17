f6e5f53b655259d5db15dfff6da6302b31dae37c
Allan Oliveira <allan_rj5@hotmail.com>
2020-11-16 22:42:42 -0300

    tela site com dados do usuario logado e opção de ir ao painel

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
@@ -7,9 +7,28 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TopoComponent implements OnInit {
 @Input() rotaLink;
-  constructor() { }
-
-  ngOnInit(): void {
+public logado: boolean = false
+dadosUsuario = JSON.parse(localStorage.getItem('userLogged'));
+  constructor(public router: Router) { 
+    
   }
 
+  ngOnInit(): void {
+    
+    console.log(this.logado)
+    if (localStorage.getItem('userLogged')) {
+      this.logado = true
+      console.log(this.logado)
+    }else{
+      this.logado = false
+      console.log(this.logado)
+    }
+      
+  }
+
+  logout() {
+    localStorage.clear();
+    window.location.reload();
+  }
+  
 }
